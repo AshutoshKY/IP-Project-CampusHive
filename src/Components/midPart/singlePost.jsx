859c7e7c837a6884dbb09f053bb77ee4f1fb7393
Ashutosh Kumar <71917947+AshutoshKY@users.noreply.github.com>
2023-08-26 13:41:59 +0530

    Update singlePost.jsx

--- src/Components/midPart/singlePost.jsx
+++ src/Components/midPart/singlePost.jsx
@@ -16,7 +16,7 @@ export default function SinglePost() {
     async function getPost(postId) {
         // console.log(postId)
         setIsLoadingPosts(true);
-        const response = await fetch(`http://localhost:5000/getPost/${postId}`).catch((err) => {
+        const response = await fetch(`https://medium-mern-clone.onrender.com/getPost/${postId}`).catch((err) => {
             console.log(err);
         });
 
